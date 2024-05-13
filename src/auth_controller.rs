d28953377cdd6fb0b5baf579dc42ebe4caa99d50
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-05-13 17:42:40 +0200

    chore: Add login functionality and registration endpoint

--- src/auth_controller.rs
+++ src/auth_controller.rs
@@ -2,6 +2,7 @@ use actix_web::HttpRequest;
 use actix_web::{get, post, web, HttpResponse, Responder};
 use sqlx::{Pool, Sqlite};
 
+use crate::auth_service;
 use crate::config::Config;
 use crate::models::User;
 use crate::responses;
@@ -20,77 +21,71 @@ pub async fn login(
     db: web::Data<Pool<Sqlite>>,
     config: web::Data<Config>,
 ) -> impl Responder {
-    // Recieves a json with username and password
-    let username = &login_info.username;
-    let password = &login_info.password;
-
-    let user_opt = match User::find_by_username(&db, username).await {
-        Ok(user) => user,
-        Err(_) => {
-            return HttpResponse::InternalServerError().json(responses::ErrorResponse {
-                error: "Failed to find user".to_string(),
-            })
-        }
-    };
-
-    let user = match user_opt {
-        Some(user) => user,
-        None => {
-            return HttpResponse::Unauthorized().json(responses::ErrorResponse {
-                error: "Invalid username or password".to_string(),
-            })
-        }
-    };
-
-    let password_hash = &user.password_hash;
-    let password_verification = utils::verify_password(password_hash, password.as_bytes());
-    if password_verification.is_err() || !password_verification.unwrap() {
-        return HttpResponse::Unauthorized().json(responses::ErrorResponse {
-            error: "Invalid username or password".to_string(),
-        });
+    match auth_service::login_user(login_info.into_inner(), &db, config.jwt_key.as_bytes()).await {
+        Ok(token) => HttpResponse::Ok().json(responses::Token { token }),
+        Err(err) => HttpResponse::InternalServerError().json(responses::ErrorResponse {
+            error: err.to_string(),
+        }),
     }
-
-    let token = match utils::generate_token(
-        &user.id,
-        utils::calculate_expiration(),
-        config.jwt_key.as_bytes(),
-    ) {
-        Ok(token) => token,
-        Err(_) => {
-            return HttpResponse::InternalServerError().json(responses::ErrorResponse {
-                error: "Failed to generate token".to_string(),
-            })
-        }
-    };
-    // }
-    if let Err(e) = user.update_last_seen(&db).await {
-        eprintln!("Failed to update last seen: {}", e);
-    }
-
-    HttpResponse::Ok().json(responses::Token { token })
 }
 
-#[post["/upload"]]
-pub async fn upload(
+#[post("/inspect")]
+pub async fn inspect(
     req: HttpRequest,
     config: web::Data<Config>,
     db: web::Data<Pool<Sqlite>>,
 ) -> impl Responder {
     match utils::validate_user(req, config.jwt_key.as_ref(), &db).await {
-        Ok(valid) if !valid => {
-            return HttpResponse::Unauthorized().json(responses::ErrorResponse {
-                error: "Token is not valid".to_string(),
-            });
-        }
-        Err(err) => {
-            return HttpResponse::InternalServerError().json(responses::ErrorResponse {
-                error: err.to_string(),
-            });
-        }
-        _ => {}
-    }
+        Ok(user) => {
+            // Obfuscate the password hash
+            let user = User {
+                password_hash: "********".to_string(),
+                ..user
+            };
 
-    HttpResponse::Ok().json(responses::Message {
-        message: "Upload successful".to_string(),
+            HttpResponse::Ok().json(user)
+        }
+        Err(err) => HttpResponse::InternalServerError().json(responses::ErrorResponse {
+            error: err.to_string(),
+        }),
+    }
+}
+
+#[post("/register")]
+pub async fn register(
+    registration_info: web::Json<responses::RegistrationRequest>,
+    req: HttpRequest,
+    db: web::Data<Pool<Sqlite>>,
+    config: web::Data<Config>,
+) -> impl Responder {
+    // Only admins can register, check if the user is an admin
+    match utils::validate_user(req, config.jwt_key.as_ref(), &db).await {
+        Ok(user) => {
+            if !user.is_admin() {
+                return HttpResponse::Unauthorized().json(responses::ErrorResponse {
+                    error: "only Admins can add users".to_string(),
+                });
+            }
+        }
+        Err(_) => {
+            return HttpResponse::InternalServerError().json(responses::ErrorResponse {
+                error: "Something went wrong".to_string(),
+            })
+        }
+    };
+
+    let (user, password) =
+        match auth_service::register_user(registration_info.into_inner(), &db).await {
+            Ok(user_password_tuple) => user_password_tuple,
+            Err(_) => {
+                return HttpResponse::InternalServerError().json(responses::ErrorResponse {
+                    error: "Failed to create user".to_string(),
+                })
+            }
+        };
+
+    HttpResponse::Ok().json(responses::LoginRequest {
+        username: user.username,
+        password,
     })
 }
